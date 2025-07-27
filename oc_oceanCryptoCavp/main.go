f6605000a5fd81b9b813a34ca931ac36793ffa4c
korea9th <105839537+korea9th@users.noreply.github.com>
2025-07-27 16:15:36 +0900

    Add files via upload
    cavp 를 위한 간단한 도구

---
+++ oc_oceanCryptoCavp/main.go
@@ -0,0 +1,102 @@
+//20250717 
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"os/exec"
+//	"strings"
+//	"bufio"
+//	"io/ioutil"
+//	"time"
+)
+
+/**
+*	execCommandStart
+**/
+func execCommandStart(name string, arg ...string) error {
+	cmd := exec.Command(name, arg...)
+	err := cmd.Start()
+	return err
+}
+
+/**
+*	execCommandRun
+**/
+func execCommandRun(name string, arg ...string) error {
+	cmd := exec.Command(name, arg...)
+	cmd.Stdout = os.Stdout
+	err := cmd.Run()
+	return err
+}
+
+/**
+*	execCommandOutput
+**/
+func execCommandOutput(name string, arg ...string) (string, error) {
+	cmd := exec.Command(name, arg...)
+	output, err := cmd.CombinedOutput()
+	return string(output), err
+}
+
+func findfilessnrun(rootpath string, alg string) (int, int) {
+
+	dirCount := 0
+	fileCount := 0
+	current_path, _ := os.Getwd()
+
+	err := filepath.Walk(rootpath, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			dirCount++
+			return nil
+		}
+//		fmt.Printf("%s, %s, %s, %s\n", alg, path, info.Name(), current_path)
+		
+		exec_err := execCommandRun(current_path + "\\" + "oceanCryptoCavp.exe", alg, path, info.Name(), current_path + "\\result" )
+//		exec_err := execCommandRun("go", "version")
+		if exec_err != nil {
+			fmt.Printf("exec error [%v]\n", exec_err)
+
+		}
+		
+		fileCount++
+		return nil
+	})
+	if err != nil {
+		fmt.Printf("walk error [%v]\n", err)
+	}
+	
+	return dirCount, fileCount
+}
+
+
+
+
+
+func main() {
+	args := os.Args
+
+	if len(args) != 3 {
+		fmt.Printf("exe req_path algorithm\n")
+		return
+	}
+	targetDir := string(args[1])
+	alg := string(args[2])
+
+	f, err := os.Open(targetDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+
+	dirs, files := findfilessnrun(targetDir, alg)
+
+
+	fmt.Printf("\n\n")
+	fmt.Printf("Total Dir : %d, file : %d\n", dirs, files);
+
+}
---
+++ oc_oceanCryptoCavp/run.bat
@@ -0,0 +1,4 @@
+echo on
+
+xcopy /y C:\00.github\oceanCrypto\oceanCryptoCavp\make_dll\x64\Release\*.exe C:\02.dev\oc_oceanCryptoCavp
+oc_oceanCryptoCavp.exe C:\07.kcvmp.or.kr\tmp hash
